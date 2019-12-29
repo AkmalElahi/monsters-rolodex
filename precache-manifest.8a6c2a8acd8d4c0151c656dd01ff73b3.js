8543aef0f6dd3e3de492f390803f33ec49891f53
akmalelahi <akmalelahishaikh@gmail.com>
2019-12-29 19:40:10 +0500

    Updates

---
+++ precache-manifest.8a6c2a8acd8d4c0151c656dd01ff73b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "069974231ca2a429879960c2b4e20e22",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e9596ab2057fe6d1c250",
+    "url": "/monsters-rolodex/static/css/main.483ebe94.chunk.css"
+  },
+  {
+    "revision": "c728ba4a2366e885ec54",
+    "url": "/monsters-rolodex/static/js/2.d8d20882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.d8d20882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e9596ab2057fe6d1c250",
+    "url": "/monsters-rolodex/static/js/main.3e0a9aed.chunk.js"
+  },
+  {
+    "revision": "dc25ba12107eb546721c",
+    "url": "/monsters-rolodex/static/js/runtime-main.75c8e44f.js"
+  }
+]);
